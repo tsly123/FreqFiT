9fd900ce400c19e2de06bd7e1a38680e177a9acc
tsly123 <35265385+tsly123@users.noreply.github.com>
2025-01-29 01:27:00 -0600

    Update gfn.py
    Add Filter2DParams

--- src/models/gfn.py
+++ src/models/gfn.py
@@ -89,3 +89,34 @@ class GlobalFilter2D(nn.Module):
         x = x.reshape(B, N, C)
 
         return x
+
+class Filter2DParams(nn.Module):
+    '''
+    Dropping the complex part of filter
+    '''
+    def __init__(self, blocks, dim, h=14, w=8):
+        super().__init__()
+        self.filter_weight = nn.Parameter(torch.randn(blocks, h, w, dim, dtype=torch.float32) * 0.02)
+        self.h = h
+        self.w = w
+        self.ssf_scale, self.ssf_shift = init_ssf_scale_shift(blocks, dim)
+
+    def forward(self, block, x, spatial_size=None):
+        B, N, C = x.shape
+        if spatial_size is None:
+            a = b = int(math.sqrt(N))
+        else:
+            a, b = spatial_size
+
+        x = x.view(B, a, b, C)
+        x = x.to(torch.float32)
+        res = x
+
+        x = torch.fft.rfft2(x, dim=(1, 2), norm='ortho')
+        x = x * self.filter_weight[block].squeeze()
+        x = torch.fft.irfft2(x, s=(a, b), dim=(1, 2), norm='ortho')
+        x = ssf_ada(x, self.ssf_scale[block], self.ssf_shift[block])
+        x = x + res
+        x = x.reshape(B, N, C)
+
+        return x
